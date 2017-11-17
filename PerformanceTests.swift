ee99da5d53c233d5424c9d9deb22e772e6ce0ea8
NIE, Shanhe <snie@samsao.co>
2017-11-17 16:33:06 -0500

    Added swift 3 support. Tested on Xcode 8.3.3

--- PerformanceTests.swift
+++ PerformanceTests.swift
@@ -5,9 +5,9 @@ class PerformanceTests: XCTestCase {
   func testCreateFromArrayRange() {
     var a = [[Double]]()
     for _ in 0..<1000 {
-      a.append([Double](count: 1000, repeatedValue: M_PI))
+      a.append([Double](repeating: Double.pi, count: 1000))
     }
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         let _ = Matrix(a, range: 0..<1000)
       }
@@ -16,7 +16,7 @@ class PerformanceTests: XCTestCase {
 
   func testTile() {
     let v = Matrix.random(rows: 1, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         let _ = v.tile(2000)
       }
@@ -25,7 +25,7 @@ class PerformanceTests: XCTestCase {
 
   func testCopyEntireMatrix() {
     let M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         let _ = M[rows: Array(0..<1000)]
       }
@@ -34,7 +34,7 @@ class PerformanceTests: XCTestCase {
 
   func testSubscriptRowGetter() {
     var M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         for i in 0..<M.rows {
           let _ = M[row: i]
@@ -46,7 +46,7 @@ class PerformanceTests: XCTestCase {
   func testSubscriptRowSetter() {
     var M = Matrix.random(rows: 1000, columns: 1000)
     let v = Matrix.random(rows: 1, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         for i in 0..<M.rows {
           M[row: i] = v
@@ -57,7 +57,7 @@ class PerformanceTests: XCTestCase {
   
   func testSubscripColumnGetter() {
     var M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         for i in 0..<M.columns {
           let _ = M[column: i]
@@ -69,7 +69,7 @@ class PerformanceTests: XCTestCase {
   func testSubscriptColumnSetter() {
     var M = Matrix.random(rows: 1000, columns: 1000)
     let v = Matrix.random(rows: 1000, columns: 1)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         for i in 0..<M.columns {
           M[column: i] = v
@@ -80,7 +80,7 @@ class PerformanceTests: XCTestCase {
 
   func testSqrt() {
     let M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       for _ in 1...10 {
         let _ = M.sqrt()
       }
@@ -89,14 +89,14 @@ class PerformanceTests: XCTestCase {
 
   func testPow2() {
     let M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       let _ = M.pow(2)
     }
   }
 
   func testPow3() {
     let M = Matrix.random(rows: 1000, columns: 1000)
-    measureBlock() {
+    measure() {
       let _ = M.pow(3)
     }
   }
